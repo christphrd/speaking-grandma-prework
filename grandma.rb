1f193dd3ed924543982eddc6f4317eaf02808203
Christopher Diep <diep.christopher@gmail.com>
2017-11-25 19:55:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase != phrase.upcase
+  else
     "HUH?! SPEAK UP, SONNY!"
   end
 end
